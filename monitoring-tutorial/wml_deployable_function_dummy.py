ec44e36d4bd82912cca67176259870fed651e16b
Wendy Wang <wanting.wang@ibm.com>
2022-04-01 15:10:27 +0000

    polished deployable function

--- monitoring-tutorial/wml_deployable_function_dummy.py
+++ monitoring-tutorial/wml_deployable_function_dummy.py
@@ -17,10 +17,8 @@ def unpickle(file):
 
 def score( input_data ):
 
-    payload = input_data.get("input_data")[0] # input_data.get("input_data") is a list
-   
-    batch_id = payload['batch_id']
-    batch_label = payload['batch_label']
+    batch_id = input_data['batch_id']
+    batch_label = input_data['batch_label']
     if batch_id in range(1,6):
         d = unpickle(f'data/cifar-10-batches-py/data_batch_{batch_id}')
 
@@ -31,15 +29,15 @@ def score( input_data ):
                 break
 
         if img_idx is None:
-            return {"predictions":[{"values":[],
-                                    "status":"Batch label not found in this batch id."}]}
+            return {"predictions":[],
+                    "status":"Batch label not found in this batch id."}
         else:
             fn = d[b'filenames'][img_idx].decode()
             label_true = d[b'labels'][img_idx]
-            return {"predictions":[{"values":[],
-                                    "status":"Found.",
-                                    "true_label":label_true,
-                                    "filename":fn}]}
+            return {"predictions":[],
+                    "status":"Found.",
+                    "true_label":label_true,
+                    "filename":fn}
     else:
-        return {"predictions":[{"values":[],
-                                "status":"Batch id does not exist. Use a number between 1 and 5."}]}
+        return {"predictions":[],
+                "status":"Batch id does not exist. Use a number between 1 and 5."}
