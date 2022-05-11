b4e6849693b213ee66f909e1f5da5a98261b203a
Wendy Wang <wanting.wang@ibm.com>
2022-05-11 00:47:33 +0000

    added concurrent request test

--- deployment-tutorial/kernel.py
+++ deployment-tutorial/kernel.py
@@ -9,11 +9,12 @@ import sys
 from io import BytesIO
 import base64
 from PIL import Image
+import time
 
-deploy_name = os.environ['REDHARE_MODEL_NAME']
-dir_user = os.environ['REDHARE_MODEL_PATH']
+
 path_model = 'model.pt'
 
+dir_user = os.environ['REDHARE_MODEL_PATH']
 dir_python_pkg = f"{dir_user}/python_packages"
 os.makedirs(dir_python_pkg,exist_ok=True)
 sys.path.insert(0, dir_python_pkg)
@@ -49,12 +50,13 @@ class Net(nn.Module):
 class MatchKernel(Kernel):
     def __init__(self):
         self.model = None
+        self.count = 0
 
     def on_kernel_start(self, kernel_context):
         
-        #Kernel.log_debug("installing packages...")
+        #print("installing packages...")
         #out = subprocess.check_output(f'pip install torch==1.10.1+cu111 -f https://download.pytorch.org/whl/torch_stable.html --target={dir_python_pkg}', shell=True, text=True, stderr=subprocess.STDOUT)
-        #Kernel.log_debug("finished installing packages")
+        #print("finished installing packages")
         
         # load model
         model = Net()
@@ -64,30 +66,30 @@ class MatchKernel(Kernel):
         
     def on_task_invoke(self, task_context):
         try:
-            Kernel.log_debug("on_task_invoke")
-
-            while task_context != None:
+            self.count += 1
+            request_id = f"{os.environ['MSD_POD_NAME']}-{self.count}"
+            print('on_task_invoke',request_id)
                 
-                # parse input
-                input_data = json.loads(task_context.get_input_data())
-                img = Image.open(BytesIO(base64.b64decode(input_data['img'])))
-                transform = transforms.ToTensor()
-                input_tensor = transform(img)
-                input_tensor = torch.stack([input_tensor])
+            # parse input
+            input_data = json.loads(task_context.get_input_data())
+            img = Image.open(BytesIO(base64.b64decode(input_data['img'])))
+            transform = transforms.ToTensor()
+            input_tensor = transform(img)
+            input_tensor = torch.stack([input_tensor])
 
-                pred = self.model(input_tensor)
-                _, pred_class = torch.max(pred, 1)
-                pred_class = int(pred_class[0].numpy())
-                
-                output_data = {'pred_class':pred_class}
-                task_context.set_output_data(json.dumps(output_data))
-                task_context = task_context.next()
+            pred = self.model(input_tensor)
+            _, pred_class = torch.max(pred, 1)
+            pred_class = int(pred_class[0].numpy())
+
+            output_data = {'pred_class':pred_class}
+            task_context.set_output_data(json.dumps(output_data))
                     
         except Exception as e:
-            Kernel.log_error(f"Failed due to {str(e)}")
-    
+            output_data = {'msg':str(e)}
+            task_context.set_output_data(json.dumps(output_data))
+
     def on_kernel_shutdown(self):
-        subprocess.run(f'rm -rf {dir_user}/*', shell=True)
+        pass
 
         
 if __name__ == '__main__':
